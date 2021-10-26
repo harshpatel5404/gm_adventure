ccad2aa3e5578909079fb6aff04dbe6f469a7946
harshpatel <harshpatel5404@gmail.com>
2021-10-26 09:28:23 +0530

    slider api done

--- lib/view/widgets/gameslider.dart
+++ lib/view/widgets/gameslider.dart
@@ -52,26 +52,68 @@ class _GameSliderState extends State<GameSlider> {
                 : 10,
             itemBuilder:
                 (BuildContext context, int itemIndex, int pageViewIndex) {
-              print("object $itemIndex");
-              return Obx(() => topgamecontroller.isLoading.value
-                  ? Container(
-                      margin: EdgeInsets.all(6.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(8.0),
-                        image: DecorationImage(
-                          image: NetworkImage(
-                              "https://kurminfotech.in/gamemania/" +
-                                  topgamecontroller
-                                      .topgamelist[itemIndex].image),
-                          fit: BoxFit.cover,
+              return Container(
+                margin: const EdgeInsets.all(5.0),
+                child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+                    child: Stack(
+                      children: <Widget>[
+                        Obx(
+                          () => topgamecontroller.isLoading.value
+                              ? Image.network(
+                                "https://kurminfotech.in/gamemania/" +
+                                    topgamecontroller
+                                        .topgamelist[itemIndex].image,
+                                fit: BoxFit.cover,
+                                width: 1000,
+                                height: 1000,
+                                
+                              )
+                              : const Center(
+                                  child: CircularProgressIndicator(),
+                                ),
                         ),
-                      ),
-                    )
-                  : const Center(child: CircularProgressIndicator()));
+                        Positioned(
+                          bottom: 0.0,
+                          left: 0.0,
+                          right: 0.0,
+                          child: Container(
+                            // ignore: prefer_const_constructors
+                            decoration: BoxDecoration(
+                              gradient: const LinearGradient(
+                                colors: [
+                                  Color.fromARGB(200, 0, 0, 0),
+                                  Color.fromARGB(0, 0, 0, 0)
+                                ],
+                                begin: Alignment.bottomCenter,
+                                end: Alignment.topCenter,
+                              ),
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            child: Obx(
+                              () => topgamecontroller.isLoading.value
+                                  ? Text(
+                                      topgamecontroller.topgamelist[itemIndex].name,
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    )
+                                  : const SizedBox(),
+                            ),
+                          ),
+                        ),
+                      ],
+                    )),
+              );
+
+            
             },
             options: CarouselOptions(
               autoPlay: true,
-              aspectRatio: 1.9,
+              aspectRatio: 1.8,
               enlargeCenterPage: true,
             ),
           ),
@@ -102,11 +144,12 @@ class _GameSliderState extends State<GameSlider> {
                   // );
                 },
                 child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.grey,
-                      borderRadius: BorderRadius.circular(25),
-                    ),
-                    child: Obx(() => adventureGameController.isLoading.value
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(25),
+                  ),
+                  child: Obx(
+                    () => adventureGameController.isLoading.value
                         ? ClipRRect(
                             borderRadius: BorderRadius.circular(25),
                             child: Image.network(
@@ -117,7 +160,9 @@ class _GameSliderState extends State<GameSlider> {
                           )
                         : const Center(
                             child: CircularProgressIndicator(),
-                          ))),
+                          ),
+                  ),
+                ),
               ),
             );
           },
